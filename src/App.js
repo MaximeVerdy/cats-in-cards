c0e5d78c4de770e2eb244301ce4c7cf2066c8829
Maxime Verdy <maximeverdy@gmail.com>
2022-04-29 22:39:29 +0200

    3ème commit - ajout de  modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from 'react'
-import './App.scss';
+import Modal from 'react-modal';
+
+import './styles/app.scss';
+import './styles/modal.scss';
 
 import Card from './components/Card'
 import catImage from './assets/shadow-cat.jpg'
 
 function App() {
 
-  const [catsArray, setCatsArray] = useState([])
+  const [catsArray, setCatsArray] = useState([]);
+  const [randomFact, setRandomFact] = useState('');
+  const [modalIsOpen, setIsOpen] = useState(false);
+
 
   useEffect(() => {
     const findCats = async () => {
@@ -15,24 +21,58 @@ function App() {
       setCatsArray(body.data)
     }
     findCats()
-
   }, [])
 
+  const handleClick = async () => {
+    const data = await fetch('https://catfact.ninja/fact')
+    const body = await data.json()
+    setRandomFact(body.fact)
+    setIsOpen(true)
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   let catsInUI = catsArray.map((cat, i) => (
-    <Card
+    <div
       key={i}
-      img={catImage}
-      title={cat.breed}
-      country={'from ' + cat.country}
-      coat= {cat.coat + ' coat'}
-      pattern= {cat.pattern + ' pattern'}
-    />
+      onClick={() => handleClick()}
+
+    >
+      <Card
+        img={catImage}
+        title={cat.breed}
+        country={'from ' + cat.country}
+        coat={cat.coat + ' coat'}
+        pattern={cat.pattern + ' pattern'}
+        onClick={() => handleClick()}
+      />
+    </div>
   ))
 
+  let modal =
+    <Modal
+      className="Modal"
+      overlayClassName="Overlay"
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel="Modal"
+      ariaHideApp={false}
+    >
+      <div>{randomFact}</div>
+    </Modal>
+
+
   return (
-    <div className="wrapper">
-      {catsInUI}
+    <div>
+      <h1>
+        CATS IN CARDS
+      </h1>
+      <div className="wrapper">
+        {catsInUI}
+        {modal}
+      </div>
     </div>
   );
 }
