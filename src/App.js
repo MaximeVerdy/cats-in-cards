7377b72abd294b6c213b4ffbc40f14afff2fdbb5
Maxime Verdy <maximeverdy@gmail.com>
2022-05-01 15:51:02 +0200

    nouvelle modal en SASS identique au projet en VueJS

--- src/App.js
+++ src/App.js
@@ -1,82 +1,74 @@
 import { useState, useEffect } from 'react'
-import Modal from 'react-modal';
 
 import './styles/app.scss';
 import './styles/modal.scss';
 
 import CatCard from './components/CatCard'
-import catImage from './assets/shadow-cat.jpg'
+import catPic from './assets/shadow-cat.jpg'
 
 function App() {
 
-  const [catsArray, setCatsArray] = useState([]);
-  const [randomFact, setRandomFact] = useState('');
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [cats, setCats] = useState([]);
+  const [fact, setFact] = useState('');
 
 
   useEffect(() => {
     const findCats = async () => {
       const data = await fetch('https://catfact.ninja/breeds');
       const body = await data.json();
-      setCatsArray(body.data);
+      setCats(body.data);
     }
     findCats()
-    .catch(e => 
-      console.error(e)
-    );
+      .catch(e =>
+        console.error(e)
+      );
   }, [])
 
-  const handleClick = async () => {
+  const seeFactInModal = async () => {
     try {
       const data = await fetch("https://catfact.ninja/fact");
       const body = await data.json();
-      setRandomFact(body.fact);
-      setIsOpen(true);
+      setFact(body.fact);
     } catch (e) {
       console.error(e);
     }
+    document.querySelector("#modal").showModal();
   }
 
-  function closeModal() {
-    setIsOpen(false);
+  const closeModal = () => {
+    document.querySelector("#modal").close();
   }
-
-  let catsInUI = catsArray.map((cat, i) => (
-    <div
-      key={i}
-      onClick={() => handleClick()}
-    >
-      <CatCard
-        img={catImage}
-        title={cat.breed}
-        coat={cat.coat + ' coat'}
-        pattern={cat.pattern + ' pattern'}
-      />
-    </div>
-  ))
-
-  let modal =
-    <Modal
-      className="Modal"
-      overlayClassName="Overlay"
-      isOpen={modalIsOpen}
-      onRequestClose={closeModal}
-      contentLabel="Modal"
-      ariaHideApp={false}
-    >
-      <div>{randomFact}</div>
-    </Modal>
 
 
   return (
     <div>
+
+      <dialog className="modal" id="modal">
+        <p>{fact}</p>
+        <button className="button close-button" onClick={() => closeModal()}>close</button>
+      </dialog>
+
       <h1>
         CATS IN CARDS
       </h1>
+
       <div className="wrapper">
-        {catsInUI}
-        {modal}
+        {cats.map((cat, i) => (
+          <div
+            key={i}
+            onClick={() => seeFactInModal()}
+          >
+            <CatCard
+              img={catPic}
+              title={cat.breed}
+              coat={cat.coat + ' coat'}
+              pattern={cat.pattern + ' pattern'}
+            />
+          </div>
+        ))}
+
       </div>
+
     </div>
   );
 }
